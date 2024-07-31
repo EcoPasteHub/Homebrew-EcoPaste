297ed566fd513f782cc80d23765b2451605cb255
ayang <473033518@qq.com>
2024-07-31 08:59:00 +0800

    feat: 更新 0.0.5 的 sha256

--- Casks/ecopaste.rb
+++ Casks/ecopaste.rb
@@ -3,10 +3,10 @@ cask "ecopaste" do
   
     if Hardware::CPU.intel?
       url "https://mirror.ghproxy.com/https://github.com/ayangweb/EcoPaste/releases/latest/download/EcoPaste_x64.app.tar.gz"
-      sha256 "e1d73d9085f78458a7c679a5fabb918b897622aeb2d3c20b91d23a3c3ec621ed"
+      sha256 "521a5340ac635744cf57cb654e904f432949ec3801d889e79f2ff0fc823e6c9d"
     else
       url "https://mirror.ghproxy.com/https://github.com/ayangweb/EcoPaste/releases/latest/download/EcoPaste_aarch64.app.tar.gz"
-      sha256 "9f3f905cadb32bb59cdbba7995097c94f220224298551d3993204b5102283976"
+      sha256 "30368d4badf299c5dbe3109d8f46234c7cea78e62a5af4c18cd4ad59f2dd6bde"
     end
   
     name "EcoPaste"
