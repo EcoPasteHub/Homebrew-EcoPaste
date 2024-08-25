46661cc8352e3c9fb6f559b8f9729edcfd2317d6
ayang <473033518@qq.com>
2024-08-25 22:29:24 +0800

    v0.0.6

--- Casks/ecopaste.rb
+++ Casks/ecopaste.rb
@@ -1,16 +1,16 @@
 cask "ecopaste" do
-    version "0.0.5"
+    version "0.0.6"
   
     if Hardware::CPU.intel?
       url "https://mirror.ghproxy.com/https://github.com/ayangweb/EcoPaste/releases/latest/download/EcoPaste_x64.app.tar.gz"
-      sha256 "521a5340ac635744cf57cb654e904f432949ec3801d889e79f2ff0fc823e6c9d"
+      sha256 "4d9bb7cc0e92bb591d6d579eb90bfacbed07cfbb93acf1bf20719464fdc8af93"
     else
       url "https://mirror.ghproxy.com/https://github.com/ayangweb/EcoPaste/releases/latest/download/EcoPaste_aarch64.app.tar.gz"
-      sha256 "30368d4badf299c5dbe3109d8f46234c7cea78e62a5af4c18cd4ad59f2dd6bde"
+      sha256 "5e2a57aeb65d24409e3b7d53c826179c4dff0ecd76d5b23300e0541c115a3d5b"
     end
   
     name "EcoPaste"
-    desc "Powerful open-source clipboard manager for MacOS and Windows platforms"
+    desc "Open source clipboard management tools for Windows, Macos and Linux."
     homepage "https://github.com/ayangweb/EcoPaste"
   
     app "EcoPaste.app"
