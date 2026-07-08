b087804e93cb157bd51b0b8838ccca835645af14
witt <52407727+witt-bit@users.noreply.github.com>
2026-07-08 20:24:25 +0800

    fix: correct release asset url (#1)

--- Casks/ecopaste.rb
+++ Casks/ecopaste.rb
@@ -5,7 +5,7 @@ cask "ecopaste" do
   sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
          intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/EcoPasteHub/EcoPaste/releases/download/v#{version}/EcoPaste_#{arch}.app.tar.gz"
+  url "https://github.com/EcoPasteHub/EcoPaste/releases/download/v#{version}/EcoPaste_#{version}_#{arch}.app.tar.gz"
   name "EcoPaste"
   desc "Open source clipboard management tool"
   homepage "https://github.com/EcoPasteHub/EcoPaste"
